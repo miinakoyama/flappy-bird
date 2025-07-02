c1f3a705ceb5d81e1e9f6758953b11702ba5a231
Miina Koyama <miinakoyama@MiinanoMacBook-Air.local>
2025-07-02 21:36:43 +0900

    initial commit

---
+++ Assets/BirdScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BirdScript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && birdIsAlive)
+        {
+            myRigidbody.linearVelocity = Vector2.up * flapStrength;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
+    }
+}
